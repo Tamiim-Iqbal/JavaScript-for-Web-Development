f4642b66c6e69745132d4e5d92cc27f24e3d8981
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2026-02-11 18:46:24 +0600

    Operators

---
+++ 02. Operators/1_arithmetic.js
@@ -0,0 +1,23 @@
+/* Arithmetic Operator: 
++, -, *, /, modulus(%), exponential(**)
+*/
+
+let a = 9;
+let b = 2;
+
+console.log("a + b :", a + b);                              //output: 11                   
+console.log("a - b :", a - b);                              //output: 7
+console.log("a * b :", a * b);                              //output: 18
+console.log("a / b :", a / b);                              //output: 4.5
+console.log("a % b :", a % b);                              //output: 2
+console.log("a ** b :", a ** b);                            //output: 81
+
+
+// Unary Operator: increment(++), decrement(--)
+a++;                    // a = a + 1
+console.log(a);                                             //output: 10
+b--;                    // b = b - 1
+console.log(b);                                             //output: 3
+
+// pre increment/decrement: ++a, --b
+// post increment/decrement: a++, b--
